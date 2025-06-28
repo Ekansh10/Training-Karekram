0d337c671e3fcabeeb20dbea2cd9861e64158a41
Ekansh10 <ekanshmahajan1010@gmail.com>
2025-06-28 10:03:24 +0530

    Day 3 Binary Search

--- Day 3/Sol1.java
+++ Day 3/Sol1.java
@@ -19,6 +19,9 @@ public class Sol1 {
 
         int avg = average(arr, N);
         System.out.println(avg);
+
+        boolean bsans = presentBS(arr, ele);
+        System.out.println(bsans);
     }
 
     static boolean present(int[] arr, int ele){
@@ -39,6 +42,28 @@ public class Sol1 {
         return ans/N;
     }
 
+    static boolean presentBS(int[] arr, int tar){
+        boolean res = false;
 
+        int r = arr.length;
+        int l = 0;
+        int mid = l +((l+r) / 2);
+
+        while(l <= r){
+            if(arr[mid] == tar){
+                res = true;
+                break;
+            }
+            else if(arr[mid] > tar){
+                r = mid -1;
+                
+            }
+            else{
+                l = mid + 1;
+            }
+            mid = l +((l+r) / 2);
+        }
+        return res;
+    }
 
 }
