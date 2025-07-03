436591be906cb3aa07c4fe42261f2b9df3f167bc
Ekansh10 <ekanshmahajan1010@gmail.com>
2025-07-03 14:46:18 +0530

    useState in functional component

--- Day 9/my-react-app/src/components/app/App.jsx
+++ Day 9/my-react-app/src/components/app/App.jsx
@@ -1,18 +1,32 @@
 import React from "react";
 import ListOfCourses from "../listofcourses/listofcourses";
 import FMessage from "../functional/message.functional";
+import Counter from "../functional/counter";
+// class App extends React.Component {
+//   render() {
+//     // return <ListOfCourses />;
+//     return (
+//       <div>
+//         <FMessage msg="hi"/>
+//         <FMessage msg="hello"/>
+//         <FMessage msg="hola"/>
+//       </div>
+//     )
+//   }
+// }
 
-class App extends React.Component {
-  render() {
-    // return <ListOfCourses />;
-    return (
+function App(){ // App as a functional component()
+  return (
       <div>
-        <FMessage msg="hi"/>
-        <FMessage msg="hello"/>
-        <FMessage msg="hola"/>
+        <Counter />
       </div>
     )
-  }
 }
+
+// var App = () => <div> // can be done this ways as well
+//         <FMessage msg="hi"/>
+//         <FMessage msg="hello"/>
+//         <FMessage msg="hola"/>
+//       </div>
 
 export default App;
