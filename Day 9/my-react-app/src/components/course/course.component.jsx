0c35082d78199ba5f101156c4a08019f37b4e534
Ekansh10 <ekanshmahajan1010@gmail.com>
2025-07-03 12:42:15 +0530

    using states

--- Day 9/my-react-app/src/components/course/course.component.jsx
+++ Day 9/my-react-app/src/components/course/course.component.jsx
@@ -1,18 +1,36 @@
 import React, { Component } from "react";
 
 export default class Course extends Component {
+  state = {currentLikes : this.props.coursedetails.likes};
+  incrementLikes(){
+    console.log("U Clicked !");
+    // this.state.currentLikes++;// states are immutable
+     
+    this.setState({currentLikes : this.state.currentLikes+1}); // it assigns new object to currentLikes state
+    console.log(this.state.currentLikes);
+  }
+
   render() {
+    let stars = this.props.coursedetails.rating;
+    let starArray = [];
+    for (let i = 0; i < stars; i++) {
+      starArray.push(<i className="fa-solid fa-star" key={i} style={{ color: "gold" }}></i>);
+    }
+    
     return (
     <div className="col-md-4">
       <div className="card">
         <img src={this.props.coursedetails.imageUrl} className="card-img-top" alt="" width="200px"/>
         <h1 className="card-title">{this.props.coursedetails.title}</h1>
         <p className="card-text">₹ {this.props.coursedetails.price}</p>
-        <p className="card-text">Rating : {this.props.coursedetails.rating}</p>
-        <button>
-            <i className="fa-solid fa-thumbs-up" width="100px" height="10px"></i>
-        </button>
-        <button className="btn btn-primary">{this.props.coursedetails.likes}</button>
+        <p className="card-text">Rating : {starArray}</p>
+
+        <button
+        onClick={()=>this.incrementLikes()
+        }
+        className="btn btn-primary" 
+        >
+        <i className="fa-solid fa-thumbs-up" width="100px" height="10px"></i> {this.state.currentLikes}</button>
       </div>
     </div>
     );
