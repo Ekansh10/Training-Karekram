a38fc0404c1dcbb70d4bf4d2f53c5cf6ad9565e1
Ekansh10 <ekanshmahajan1010@gmail.com>
2025-07-07 15:05:27 +0530

    reduce

--- Day 13/functionalProgramming.py
+++ Day 13/functionalProgramming.py
@@ -1,4 +1,5 @@
 # map is the function to change the every element from the iterator
+import functools
 
 nums = [10,20,30, 3, 7, 9]
 
@@ -8,3 +9,9 @@ print(sq)
 
 sq = list(filter(lambda x : x%2 == 0, nums))
 print(sq)
+
+
+# reduce function give summary of the iterable item
+
+s = functools.reduce(lambda x, y: x + y, nums) # sum of all elements in iterable item
+print(s)
