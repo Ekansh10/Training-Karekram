408e76158e923d66cf576d04979b23b6e9838135
Ekansh10 <ekanshmahajan1010@gmail.com>
2025-07-07 15:52:29 +0530

    checking if the array is in acending order

--- Day 13/functionalProgramming.py
+++ Day 13/functionalProgramming.py
@@ -15,3 +15,9 @@ print(sq)
 
 s = functools.reduce(lambda x, y: x + y, nums) # sum of all elements in iterable item
 print(s)
+
+lst = [1,2,10,4]
+
+res = functools.reduce(lambda x, y: (y, x[1] and x[0] <= y) if type(x) == tuple else (y, x <= y), lst)
+
+print(res)
