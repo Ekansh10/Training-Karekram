c487bda4a67feb19ac93f11c98bb1c66838d34f6
Ekansh10 <ekanshmahajan1010@gmail.com>
2025-07-07 16:59:19 +0530

    r+, w+, a+ modes

--- Day 13/fileoperations.py
+++ Day 13/fileoperations.py
@@ -10,5 +10,32 @@ with open("abc.txt", 'r') as f:
     f.seek(10) # skips n bytes,  1 byte = 1 char
     print(f.readlines()) # returns list of lines
 
+with open("abc.txt", 'r+') as f: # read behaviour but write at the eof
+    print(f.readline())
+    print(f.readline())
+    f.write("This line is from r+ mode\n") # write data at the end of the file
+    print(f.read()) # blank ouytput since the file pointer was at the end after write()
 
+
+with open("xyz.txt", 'w+') as f: # this will blank the file first and then write
+    # replaced the old content
+    print(f.read())
+    f.write("This line is from w+")
+    print(f.read())
+    f.seek(0) # this will take the file pointer at the oth byte
+    f.write("###") #This will start replacing the bytes from the location of the file pointer
+    f.seek(0)
+    print(f.read()) 
+    print(f.read()) # here nothing will be read as the fp will be at the eof
 # read write append mode
+
+with open("xyz.txt", 'a+') as f: # this will blank the file first and then write
+    # replaced the old content
+    print(f.read())
+    f.write("This line is from w+")
+    print(f.read())
+    f.seek(0) # this will take the file pointer at the oth byte
+    f.write("###") #This will start replacing the bytes from the location of the file pointer
+    f.seek(0)
+    print(f.read()) 
+    print(f.read()) # here nothing will be read as the fp will be at the eof
