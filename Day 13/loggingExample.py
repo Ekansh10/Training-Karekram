8aaa3a822f22ae44d7b6e9f354ce4192caea138a
Ekansh10 <ekanshmahajan1010@gmail.com>
2025-07-12 10:45:35 +0530

    setting handler levels

--- Day 13/loggingExample.py
+++ Day 13/loggingExample.py
@@ -6,11 +6,16 @@ logger = logging.getLogger("My logger")
 logger.setLevel(logging.INFO) # Here we will not record levels below INFO level. But Info and above levels will be recorded
 
 
+# Remove all existing handlers
+if logger.hasHandlers():
+    logger.handlers.clear()
 
 # Logging levels: NOTEST(0)/DEBUG(10)/INFO(20)/WARNING(30)/ERROR(40)/CRITICAL(50)
 stream_handler = logging.StreamHandler()
 dev_file_handler = logging.FileHandler("app_dev.log")
+dev_file_handler.setLevel(logging.CRITICAL) # will only log critical logs
 test_file_handler = logging.FileHandler("app_test.log")
+
 
 logger.addHandler(stream_handler)
 logger.addHandler(dev_file_handler)
@@ -19,3 +24,4 @@ logger.addHandler(test_file_handler)
 logger.info("App Started")
 logger.debug("This is debug message")
 logger.warning("This is warning")
+logger.critical("!! This is Critical !!")
